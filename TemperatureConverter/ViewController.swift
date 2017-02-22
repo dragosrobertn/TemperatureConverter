9568a1710f2c0fc87c5a7298addf1d5b3d3a4ec4
dragosrobertn <hello@dragosneagu.com>
2017-02-22 04:01:50 +0000

    Add UI elements, constraints, functions for conversions.
    This commit contains all the bits needed for the Temperature Converter:
    - UI elements on the Storyboard
    - Constraints and UI properties
    - Outlets and actions associated to UI elements
    - Functions for converting and calculating grades
    - A conversion counter

--- TemperatureConverter/ViewController.swift
+++ TemperatureConverter/ViewController.swift
@@ -10,6 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var temperatureEntry: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var conversionsCounter: UILabel!
+    
+    var conversions = 0
+
+    @IBAction func convertCelsiusClicked(_ sender: Any) {
+        convert(scale: "Celsius")
+    }
+    
+    @IBAction func convertFahrenheitClicked(_ sender: Any) {
+        convert(scale: "Fahrenheit")
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +35,33 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func calcGrades (type: String, value: Double) -> Double {
+        switch type {
+        case "Fahrenheit":
+            return value * (9/5) + 32
+        case "Celsius":
+            return (value - 32) / (9/5)
+        default:
+            return 0.0
+        }
+    }
+    
+    func convert(scale: String){
+        if let result = temperatureEntry.text {
+            
+            if (result != "") {
+                if let num = Double(result){
+                    let output = calcGrades(type: scale, value: num)
+                    resultLabel.text = String(output)
+                    conversions += 1
+                    conversionsCounter.text = "Conversions: " + String(conversions)
+                }
+            }
+            else {
+                return
+            }
+        }
 
+    }
 }
 
